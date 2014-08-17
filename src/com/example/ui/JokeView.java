56dae7d70df346f53afd343aee2653afda8338c8
divesli <divesli@163.com>
2014-08-17 21:40:30 +0800

    update

--- src/com/example/ui/JokeView.java
+++ src/com/example/ui/JokeView.java
@@ -23,6 +23,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 
 import android.view.View.OnClickListener;
 import android.widget.EditText;
@@ -53,6 +54,7 @@ public class JokeView extends ActionBarActivity implements OnClickListener{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_joke_view);
 		
 		Intent intent = getIntent();
